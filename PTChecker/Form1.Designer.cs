9e6b37f3e87b2c2c09bb2e14841e4dc34819fe59
Gidsik <gidsik@mail.ru>
2019-08-20 22:27:44 +0700

    Добавьте файлы проекта.

---
+++ PTChecker/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace PTChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(116, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "none";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(116, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "none";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(116, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "none";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(116, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "none";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Run Time :";
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 22);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Close Time :";
+            this.label6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(12, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 22);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Start Time :";
+            this.label7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 22);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "ProcessName :";
+            this.label8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(12, 1);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(260, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Photoshop Time";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_MouseDown);
+            this.label9.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 134);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PTChecker";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ PTChecker/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTChecker
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        System.Diagnostics.Process PS;
+        Boolean isMouseDown = false;
+        Point MouseOffset;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            thread = new Thread(
+                    () => {
+                        while (true)
+                        {
+                            if (PSOnCheck())
+                            {
+                                DateTime start = PS.StartTime;
+                                DateTime end;
+                                Action action = () => {
+                                    label1.Text = PS.ProcessName;
+                                    label2.Text = PS.StartTime.ToString();
+                                    if (PS.HasExited) {
+                                        end = System.DateTime.Now;
+                                        label3.Text = end.ToString();
+                                        TimeSpan dt2 = end - start;
+                                        label4.Text = dt2.ToString();
+                                    }
+                                };
+                                label1.Invoke(action);
+                                
+                            }
+                            Thread.Sleep(100);
+                        }
+                    }
+                );
+            thread.Start();
+
+        }
+
+        Boolean PSOnCheck()
+        {
+            System.Diagnostics.Process[] psProcess = System.Diagnostics.Process.GetProcessesByName("Photoshop");
+
+            MyProcess[] processes = (MyProcess[])System.Diagnostics.Process.GetProcesses()
+                .Where(x => x.ProcessName == "Photoshop");
+
+            foreach (MyProcess process in processes)
+            {
+                Console.Write(process.ProcessName);
+            }
+
+            if (psProcess.Length > 0)
+            {
+                PS = psProcess[0];
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread.Abort();
+            Application.Exit();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = -e.X;
+                int y = -e.Y;
+                isMouseDown = true;
+                MouseOffset = new Point(x, y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(MouseOffset);
+                Location = mousePos;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (MessageBox.Show("Выйти?", "Выход", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void Control_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = -e.X - ((Control)sender).Location.X;
+                int y = -e.Y - ((Control)sender).Location.Y;
+                isMouseDown = true;
+                MouseOffset = new Point(x, y);
+            }
+        }
+    }
+}
